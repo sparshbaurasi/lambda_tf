ebea7f60e052eeee1d0c4a7a08bd3da7653c431a
sparshbaurasi <107546419+sparshbaurasi@users.noreply.github.com>
2024-02-23 12:44:28 +0530

    Done

--- examples/lambda/code/downscaler/downscaler.py
+++
@@ -1,8 +0,0 @@
-import boto3
-region = 'us-east-1'
-instances = ['i-06fa0772b0743689f', 'i-00e7927756ce6653b']
-ec2 = boto3.client('ec2', region_name=region)
-
-def lambda_handler(event, context):
-    # ec2.stop_instances(InstanceIds=instances)
-    print('stopped your instances: ' + str(instances))
